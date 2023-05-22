f4322d6abde6223ef7e70b2c07874a092651653e
CobyPear <coby.sher@pantheon.io>
2023-05-22 11:10:36 -0500

    Reproduction for broken router in pages dir

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,12 @@
+import "@pantheon-systems/nextjs-kit/style.css";
+import { ContentWithImage } from "@pantheon-systems/nextjs-kit";
+import Link from "next/link";
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return (
+    <>
+      <Link href="/paginator">to paginator page</Link>
+      <ContentWithImage content="My Content" title="test" />
+    </>
+  );
 }
---
+++ pages/paginator/[[...page]].tsx
@@ -0,0 +1,32 @@
+import "@pantheon-systems/nextjs-kit/style.css";
+import { Paginator } from "@pantheon-systems/nextjs-kit";
+import Link from "next/link";
+/** Add your relevant code here for the issue to reproduce */
+export default function PaginatorPage() {
+  return (
+    <div
+      style={{
+        width: "1024px",
+        display: "flex",
+        flexDirection: "column",
+        marginRight: "auto",
+        marginLeft: "auto",
+      }}
+    >
+      <Link style={{ marginBottom: "48px" }} href="/">
+        Home
+      </Link>
+      <Paginator
+        data={[{ title: "hi" }, { title: "hello" }, { title: "oy" }]}
+        itemsPerPage={1}
+        routing={true}
+        breakpoints={{ start: 0, end: 3, add: 0 }}
+        Component={({ currentItems }) =>
+          currentItems.map(({ title }: { title: string }) => (
+            <div key={title}>{title}</div>
+          ))
+        }
+      />
+    </div>
+  );
+}
